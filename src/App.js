24a7be118c013a16a268e09a1e12b15d41d552c7
Puyan Wei <puyanwei@gmail.com>
2018-01-25 15:55:56 +0000

    applied dev coffee code that I was at to repo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component {
+class HelloWorld extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div
+        className="App"
+        style={{
+          backgroundColor: "red",
+          fontSize: 33 / 2
+        }}
+        id="hello-world"
+      >
+        <h2>Hello {this.props.name}</h2>
+        <h2>This is a section of the component</h2>
       </div>
     );
   }
 }
 
-export default App;
+export default HelloWorld;
+
+// class HelloWorld extends React.Component{
+//   render(){ //returns jsx
+//
+//     return (
+//
+//     );
+//   }
+// }
+//
+// ReactDOM.render(<HelloWorld name="Chris"/>, document.getElementById("app"))
+//
+// //props = properties, style, className, state
