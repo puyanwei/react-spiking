b64912465b7cc2b5f11e0fdb2a89136703fadddc
Puyan Wei <puyanwei@gmail.com>
2018-01-25 16:20:28 +0000

    added css styling

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import "./App.css";
 
 class HelloWorld extends Component {
   render() {
+    const styleObj = {
+      backgroundColor: "red"
+    };
+
     return (
-      <div
-        className="App"
-        style={{
-          backgroundColor: "red",
-          fontSize: 33 / 2
-        }}
-        id="hello-world"
-      >
+      <div className="App" style={styleObj} id="hello-world">
         <h2>Hello {this.props.name}</h2>
         <h2>This is a section of the component</h2>
       </div>
@@ -20,16 +17,3 @@ class HelloWorld extends Component {
 }
 
 export default HelloWorld;
-
-// class HelloWorld extends React.Component{
-//   render(){ //returns jsx
-//
-//     return (
-//
-//     );
-//   }
-// }
-//
-// ReactDOM.render(<HelloWorld name="Chris"/>, document.getElementById("app"))
-//
-// //props = properties, style, className, state
